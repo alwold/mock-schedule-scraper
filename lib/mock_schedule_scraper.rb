c1e52607a7859c6737561cb947d3021239dce47c
Al Wold <alwold@alwold.com>
2014-09-09 23:37:26 -0700

    modify behavior to allow special data to be passed in to get an invalid class, or closed/open class

--- lib/mock_schedule_scraper.rb
+++ lib/mock_schedule_scraper.rb
@@ -3,28 +3,28 @@ require 'json'
 require_relative 'mock_class_info'
 
 class MockScheduleScraper
+  # any term that starts with 2 is valid, and class numbers with 5 digits are accepted
+  # if those two rules are met, fake info will be provided
+  # otherwise, nil is returned
   def get_class_info(term_code, class_number)
-    # course_info = fetch(term_code, class_number)
-
-    # if course_info.nil?
-    #   nil
-    # else
-    #   MockClassInfo.new(course_info['name'], course_info['schedule'])
-    # end
-    MockClassInfo.new('Fake Course', 'MWF')
+    if term_code.start_with?('2') && class_number.length == 5
+      MockClassInfo.new('Fake Course', 'MWF')
+    else
+      nil
+    end
   end
 
-
+  # any term that starts with 2 is valid, and class numbers with 5 digits are accepted
+  # if those two rules are met, class numbers starting with 0-4 will return :open, 5-9 will return :closed
+  # if class is invalid, nil is returned
   def get_class_status(term_code, class_number)
-    # course_info = fetch(term_code, class_number)
-    # if course_info.nil?
-    #   nil
-    # elsif course_info['status'] == "Open"
-    #   :open
-    # else
-    #   :closed
-    # end
-    :closed
+    if !term_code.start_with?('2') || class_number.length != 5
+      nil
+    elsif class_number.match(/^[0-4].+/)
+      :open
+    else
+      :closed
+    end
   end
 
   private
